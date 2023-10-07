929cff5b7351ef7c59ffea9111b47e051ea1607a
Semih Gül <semih.gul099@gmail.com>
2023-10-07 15:20:18 +0300

    recycler view bağlantısı sağlandı

--- app/src/main/java/com/example/myimdbbook/model/Movie.java
+++ app/src/main/java/com/example/myimdbbook/model/Movie.java
@@ -1,14 +1,24 @@
 package com.example.myimdbbook.model;
 
 public class Movie {
+    private int id;
     private String movieName;
     private String date;
     private String score;
 
-    public Movie(String movieName, String date, String score) {
+    public Movie(int id,String movieName, String date, String score) {
+        this.id=id;
         this.movieName = movieName;
         this.date = date;
         this.score = score;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getMovieName() {
--- app/src/main/java/com/example/myimdbbook/views/MainFragment.java
+++ app/src/main/java/com/example/myimdbbook/views/MainFragment.java
@@ -1,10 +1,17 @@
 package com.example.myimdbbook.views;
 
+import static com.example.myimdbbook.database.DbHelper.TABLE_NAME;
+
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,10 +19,18 @@ import android.view.ViewGroup;
 
 import com.example.myimdbbook.R;
 import com.example.myimdbbook.adapters.MovieAdapter;
+import com.example.myimdbbook.database.DbHelper;
 import com.example.myimdbbook.databinding.FragmentMainBinding;
+import com.example.myimdbbook.model.Movie;
+
+import java.util.ArrayList;
 
 public class MainFragment extends Fragment {
     private FragmentMainBinding binding;
+    MovieAdapter adapter;
+    ArrayList<Movie> movieArrayList;
+    SQLiteDatabase database;
+    DbHelper dbHelper;
 
     public MainFragment() {
         // Required empty public constructor
@@ -30,6 +45,8 @@ public class MainFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        movieArrayList = new ArrayList<>();
+        dbHelper = new DbHelper(getContext());
     }
 
     @Override
@@ -44,5 +61,37 @@ public class MainFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        // RecyclerView'i başlatma
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
+        adapter = new MovieAdapter(movieArrayList, getContext());
+        recyclerView.setAdapter(adapter);
+
+        // Verileri al
+        getData();
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private void getData() {
+        movieArrayList.clear();
+        database = dbHelper.getReadableDatabase();
+        Cursor cursor = database.rawQuery("select * from " + TABLE_NAME, null);
+
+        int idx = cursor.getColumnIndex("id");
+        int moviex = cursor.getColumnIndex("movieName");
+        int datex = cursor.getColumnIndex("finishDate");
+        int scorex = cursor.getColumnIndex("score");
+
+        while (cursor.moveToNext()) {
+            int id = cursor.getInt(idx);
+            String movie = cursor.getString(moviex);
+            String date = cursor.getString(datex);
+            String score = cursor.getString(scorex);
+
+            Movie movie1 = new Movie(id, movie, date, score);
+            movieArrayList.add(movie1);
+        }
+        adapter.notifyDataSetChanged();
+        cursor.close();
     }
 }
